7a5f57117ac5a7c63a47ab72b7def0268d62e481
dana tinnerello <d.tinnerello@alumno.um.edu.ar>
2025-04-28 09:27:24 -0300

    implementacion de la funcion factorial recursiva correcta

--- src/factorial.py
+++ src/factorial.py
@@ -1,14 +1,24 @@
 #funcion factorial NO recursiva
 
 
-def factorial_NO_recursiva(n:int)->int:
-    total = 1
-    while n > 0:
-        total = total * n
-        n = n - 1 
-    return total 
+# def factorial_NO_recursiva(n:int)->int:
+#     total = 1
+#     while n > 0:
+#         total = total * n
+#         n = n - 1 
+#     return total 
+
+#funcion factorial recursiva
+
+def factorial_recursiva(n:int):
+    if n ==1 or n ==0:
+        return 1 
+    else:
+        return n* factorial_recursiva(n-1)
+    
 
 n = int(input("ingrese un numero"))
-result = factorial_NO_recursiva(n)
+result = factorial_recursiva(n)
 print("el factorial es: ",result)
+
 
